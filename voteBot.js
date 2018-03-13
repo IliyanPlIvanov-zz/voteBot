dd34abee1a8afe2aa16877c0c8dc409286816738
Iliyan Ivanov <iliyan.pl.ivanov@gmail.com>
2018-03-13 17:56:25 +0200

    upload

---
+++ voteBot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+	if (msg.author.bot) return;
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+
+});
+
+client.login(process.env.BOT_TOKEN);
