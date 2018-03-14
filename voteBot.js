9534b31936bad5ba9d84f11479a1d8bd91215943
Iliyan Ivanov <iliyan.pl.ivanov@gmail.com>
2018-03-14 15:16:27 +0200

    no need of console.logs

--- voteBot.js
+++ voteBot.js
@@ -3,9 +3,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+/*client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-});
+});*/
 
 client.on('message', async msg => {
 	if (msg.author.bot) return;
