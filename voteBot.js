c2f7304ce25622b42602b00e371e83bf1ac805fe
Iliyan Ivanov <iliyan.pl.ivanov@gmail.com>
2018-03-14 15:28:57 +0200

    feedback to different words

--- voteBot.js
+++ voteBot.js
@@ -11,6 +11,14 @@ client.on('message', async msg => {
 	if (msg.author.bot) return;
 	if (msg.content === 'ping') {
 		msg.reply('pong');
+	} else if (msg.content === 'hi') {
+		msg.reply('hello');
+	} else if (msg.content === 'hello') {
+		msg.reply('hi');
+	}  else if (msg.content === 'bye') {
+		msg.reply('bye');
+	}  else if (msg.content === 'fuck') {
+		msg.reply("don't swear!");
 	}
 
 });
